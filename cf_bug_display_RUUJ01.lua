a22c7aa3800a51c88d27ae8832e5d1bdb82a3ce1
DrewPag <robertdpaganetti@gmail.com>
2024-06-09 17:21:02 -0400

    Added TTL to display

--- cf_bug_display_RUUJ01.lua
+++ cf_bug_display_RUUJ01.lua
@@ -1,4 +1,5 @@
 local bugAddrs = {0x902DE0B0, 0x902DF130, 0x902E01B0, 0x902E1230, 0x902E22B0, 0x902E3330, 0x902E43B0, 0x902E5430, 0x902E64B0, 0x902E7530, 0x902E85B0, 0x902E9630}
+local ttlAddrs = {0x902DE0B4, 0x902DF134, 0x902E01B4, 0x902E1234, 0x902E22B4, 0x902E3334, 0x902E43B4, 0x902E5434, 0x902E64B4, 0x902E7534, 0x902E85B4, 0x902E9634}
 
 local bugNames = {"Common Butterfly", "Yellow Butterfly", "Tiger Butterfly", "Peacock Butterfly",
         "Monarch Butterfly", "Emperor Butterfly", "Agrias Butterfly", "Raja Brooke", "Birdwing", "Moth", "Oak Silk Moth", "Honeybee",
@@ -29,22 +30,26 @@ function onScriptUpdate()
     for i, bugAddr in pairs(bugAddrs) do
         bugId = ReadValue32(bugAddr)
         local name = "255 not filled"
+        local ttlSeconds = -1
 
         local bugRelFlag = 0x74
         local bugRelFlagValue = ReadValue32(bugFlagAddr)
+        local ttlMs = ReadValue32(ttlAddrs[i])
 
         if bugRelFlagValue == bugRelFlag then
             if bugId <= 64 then
                 name = bugNames[bugId+1]
+                ttlSeconds = math.floor(ttlMs / 60)
             else
                 name = "N/A"
+                ttlSeconds = -1
             end
         else
             WriteValue32(bugAddr, 65)
             name = "N/A"
+            ttlSeconds = -1
         end
-    
-        text = text .. "Bug " .. i .. ": " .. name .. "\n"
+        text = text .. "Bug " .. i .. ": " .. name .. "\nTTL: " .. ttlSeconds .. "\n\n"
 
     end
 
